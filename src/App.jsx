ee2b2d6607fc6078dbc98fa677574931f453df06
brunosv <brunosv89@hotmail.com>
2023-09-14 20:15:22 -0300

    uping

---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { TextArea, Button, Select, TextField } from "bold-ui";
+import forge from 'node-forge';
+import {
+  encodeCesar,
+  encodeVernam,
+  encodeVigenere,
+  decodeVigenere,
+  encodePlayfair,
+  decodePlayfair,
+  encodeHill,
+  decodeHill,
+  encodeRSA,
+  decodeRSA   
+} from "./cyphers-functions";
+
+function App() {
+  const items = ["Cesar", "Vernam", "Vigenère", "Playfair", "Hill", "RSA"];
+  const [value, setValue] = useState("");
+  const [msg, setMsg] = useState("");
+  const [resultado, setResultado] = useState("");
+  const [cesarKey, setCesarKey] = useState(3);
+  const [vernamKey, setVernamKey] = useState("");
+  const [vigenereKey, setVigenereKey] = useState("");
+  const [playfairKey, setPlayfairKey] = useState("");
+  const hillMatrizKey = [
+    [6, 24, 1],
+    [13, 16, 10],
+    [20, 17, 15],
+  ];
+  
+  const handleChangeValue = (item) => setValue(item);
+  const handleChangeMsg = (event) => setMsg(event.target.value);
+  const handleChangeCesarKey = (event) => setCesarKey(event.target.value);
+  const handleChangeVernamKey = (event) => setVernamKey(event.target.value);
+  const handleChangeVigenereKey = (event) => setVigenereKey(event.target.value);
+  const handleChangePlayfairKey = (event) => setPlayfairKey(event.target.value);
+
+  const style = {
+    button: {
+      backgroundColor: "rgb(71, 147, 218)",
+      color: "rgb(255, 255, 255)",
+      width: "130px",
+    },
+  };
+
+
+
+  const encode = () => {
+    switch (value) {
+      case "Cesar":
+        setResultado(encodeCesar(msg, cesarKey));
+        break;
+      case "Vernam":
+        setResultado(encodeVernam(msg, vernamKey));
+        break;
+      case "Vigenère":
+        setResultado(encodeVigenere(msg, vigenereKey));
+        break;
+      case "Playfair":
+        setResultado(encodePlayfair(msg, vigenereKey));
+        break;
+      case "Hill":
+        setResultado(encodeHill(msg, hillMatrizKey, 26));
+        break;
+      case "RSA": 
+        //setResultado(encodeRSA(msg, chavePublica));
+        break;
+      default:
+        console.log("fail");
+    }
+  };
+
+  const decode = () => {
+    switch (value) {
+      case "Cesar":
+        setResultado(encodeCesar(msg, -cesarKey));
+        break;
+      case "Vernam":
+        setResultado(encodeVernam(msg, vernamKey));
+        break;
+      case "Vigenère":
+        setResultado(decodeVigenere(msg, vigenereKey));
+        break;
+      case "Playfair":
+        setResultado(decodePlayfair(msg, vigenereKey));
+        break;
+      case "Hill":
+        setResultado(decodeHill(msg, hillMatrizKey, 26));
+        break;
+      case "RSA":        
+        setResultado(decodeRSA(msg, keys.privateKey));
+        break;
+      default:
+        console.log("fail");
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Cyphers</p>
+      </header>
+      <div className="App-body">
+        <TextArea
+          label="Digite a mensagem: "
+          style={{ width: "400px", height: "80px" }}
+          onChange={handleChangeMsg}
+          value={msg}
+        />
+      </div>
+      <div className="App-select">
+        <Select
+          items={items}
+          placeholder="Selecione a cifra"
+          value={value}
+          onChange={handleChangeValue}
+          required
+        />
+      </div>
+      {value === "Cesar" && (
+        <div className="App-cesarKey">
+          <TextField
+            label="Chave"
+            onChange={handleChangeCesarKey}
+            placeholder={cesarKey}
+            required
+          />
+        </div>
+      )}
+      {value === "Vernam" && (
+        <div className="App-vernamKey">
+          <TextArea
+            label="Informe a chave: "
+            style={{ width: "400px", height: "40px" }}
+            onChange={handleChangeVernamKey}
+            value={vernamKey}
+            required
+          />
+        </div>
+      )}
+      {value === "Vigenère" && (
+        <div className="App-vigenereKey">
+          <TextArea
+            label="Informe a chave: "
+            style={{ width: "400px", height: "40px" }}
+            onChange={handleChangeVigenereKey}
+            value={vigenereKey}
+            required
+          />
+        </div>
+      )}
+      {value === "Playfair" && (
+        <div className="App-playfairKey">
+          <TextField
+            label="Chave"
+            onChange={handleChangePlayfairKey}
+            placeholder={playfairKey}
+            style={{ width: "400px" }}
+            required
+          />
+        </div>
+      )}
+      <div className="App-buttons">
+        <Button style={style.button} onClick={encode}>
+          Criptografar
+        </Button>
+        <Button style={style.button} onClick={decode}>
+          Descriptografar
+        </Button>
+      </div>
+      <div className="App-result">
+        <p> Resultado: </p>
+        <p> {resultado} </p>
+      </div>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cyphers-functions.jsx
@@ -0,0 +1,346 @@
+export const encodeCesar = (msg, deslocamento) => {
+  let resultado = "";
+
+  for (let i = 0; i < msg.length; i++) {
+    let char = msg[i];
+
+    if (char.match(/[a-z]/i)) {
+      const codigoAtual = msg.charCodeAt(i);
+      let codigoCriptografado;
+
+      if (char.match(/[a-z]/)) {
+        codigoCriptografado = ((codigoAtual - 97 + deslocamento) % 26) + 97;
+      } else if (char.match(/[A-Z]/)) {
+        codigoCriptografado = ((codigoAtual - 65 + deslocamento) % 26) + 65;
+      }
+
+      resultado += String.fromCharCode(codigoCriptografado);
+    } else {
+      resultado += char;
+    }
+  }
+
+  return resultado;
+};
+
+export const encodeVernam = (msg, chave) => {
+  let mensagemCriptografada = "";
+  let resultado = "";
+  for (let i = 0; i < msg.length; i++) {
+    const mensagemCharCode = msg.charCodeAt(i) - 65; // Converter para número (A=0, B=1, ..., Z=25)
+    const chaveCharCode = chave.charCodeAt(i) - 65;
+
+    // Aplicar a operação XOR módulo 26
+    const resultadoCharCode = (mensagemCharCode ^ chaveCharCode) % 26;
+
+    // Converter o resultado de volta para caractere
+    mensagemCriptografada += String.fromCharCode(resultadoCharCode + 65); // Converter de volta para caractere maiúsculo
+    resultado = mensagemCriptografada.toLowerCase();
+  }
+
+  return resultado;
+};
+
+export const encodeVigenere = (mensagem, chave) => {
+  mensagem = mensagem.toUpperCase(); // Converter a mensagem para maiúsculas
+  chave = chave.toUpperCase(); // Converter a chave para maiúsculas
+
+  let mensagemCriptografada = "";
+  let chaveIndex = 0;
+
+  for (let i = 0; i < mensagem.length; i++) {
+    const mensagemChar = mensagem.charAt(i);
+    const mensagemCharCode = mensagemChar.charCodeAt(0) - 65; // Converter de A=0, B=1, ..., Z=25
+
+    if (mensagemCharCode >= 0 && mensagemCharCode <= 25) {
+      const chaveChar = chave.charAt(chaveIndex % chave.length);
+      const chaveCharCode = chaveChar.charCodeAt(0) - 65; // Converter de A=0, B=1, ..., Z=25
+
+      const resultadoCharCode = (mensagemCharCode + chaveCharCode) % 26; // Aplicar a cifra
+
+      const resultadoChar = String.fromCharCode(resultadoCharCode + 65); // Converter de volta para o caractere
+      mensagemCriptografada += resultadoChar;
+
+      chaveIndex++;
+    } else {
+      // Se o caractere não for uma letra, mantenha-o inalterado
+      mensagemCriptografada += mensagemChar;
+    }
+  }
+
+  return mensagemCriptografada.toLowerCase();
+};
+
+export const decodeVigenere = (mensagem, chave) => {
+  mensagem = mensagem.toUpperCase();
+  chave = chave.toUpperCase();
+
+  let mensagemOriginal = "";
+  let chaveIndex = 0;
+
+  for (let i = 0; i < mensagem.length; i++) {
+    const mensagemChar = mensagem.charAt(i);
+    const mensagemCharCode = mensagemChar.charCodeAt(0) - 65;
+
+    if (mensagemCharCode >= 0 && mensagemCharCode <= 25) {
+      const chaveChar = chave.charAt(chaveIndex % chave.length);
+      const chaveCharCode = chaveChar.charCodeAt(0) - 65;
+
+      const resultadoCharCode = (mensagemCharCode - chaveCharCode + 26) % 26; // Aplicar a decifra
+
+      const resultadoChar = String.fromCharCode(resultadoCharCode + 65);
+      mensagemOriginal += resultadoChar;
+
+      chaveIndex++;
+    } else {
+      mensagemOriginal += mensagemChar;
+    }
+  }
+
+  return mensagemOriginal.toLowerCase();
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/////// CIFRA DE PLAYFAIR
+
+function createPlayfairMatrix(key) {
+  const matrix = Array(5)
+    .fill(null)
+    .map(() => Array(5).fill(null));
+  const keySet = new Set();
+  let alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // Excluir a letra J
+
+  key = key.toUpperCase().replace(/J/g, "I"); // Substituir J por I e converter para maiúsculas
+
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < key.length; i++) {
+    const letter = key[i];
+    if (!keySet.has(letter)) {
+      matrix[row][col] = letter;
+      keySet.add(letter);
+
+      if (col === 4) {
+        col = 0;
+        row++;
+      } else {
+        col++;
+      }
+    }
+  }
+
+  for (let i = 0; i < 25; i++) {
+    const letter = alphabet[i];
+    if (!keySet.has(letter)) {
+      matrix[row][col] = letter;
+
+      if (col === 4) {
+        col = 0;
+        row++;
+      } else {
+        col++;
+      }
+    }
+  }
+
+  return matrix;
+}
+
+function findLetterPosition(matrix, letter) {
+  for (let row = 0; row < 5; row++) {
+    for (let col = 0; col < 5; col++) {
+      if (matrix[row][col] === letter) {
+        return { row, col };
+      }
+    }
+  }
+  return null;
+}
+
+export const encodePlayfair = (msg, key) => {
+  const matrix = createPlayfairMatrix(key);
+  msg = msg.toUpperCase().replace(/J/g, "I"); // Substituir J por I e converter para maiúsculas
+  msg = msg.replace(/[^A-Z]/g, ""); // Remover caracteres não alfabéticos
+  let ciphertext = "";
+
+  for (let i = 0; i < msg.length; i += 2) {
+    let pair = msg.slice(i, i + 2);
+
+    if (pair.length === 1) {
+      pair += "X"; // Adicionar X se o par tiver apenas uma letra
+    }
+
+    const pos1 = findLetterPosition(matrix, pair[0]);
+    const pos2 = findLetterPosition(matrix, pair[1]);
+
+    let encryptedPair = "";
+
+    if (pos1.row === pos2.row) {
+      encryptedPair += matrix[pos1.row][(pos1.col + 1) % 5];
+      encryptedPair += matrix[pos2.row][(pos2.col + 1) % 5];
+    } else if (pos1.col === pos2.col) {
+      encryptedPair += matrix[(pos1.row + 1) % 5][pos1.col];
+      encryptedPair += matrix[(pos2.row + 1) % 5][pos2.col];
+    } else {
+      encryptedPair += matrix[pos1.row][pos2.col];
+      encryptedPair += matrix[pos2.row][pos1.col];
+    }
+
+    ciphertext += encryptedPair;
+  }
+
+  return ciphertext.toLowerCase();
+};
+
+export const decodePlayfair = (msg, key) => {
+  const matrix = createPlayfairMatrix(key);
+  let plaintext = "";
+  let cypherMsg = msg.toUpperCase();
+
+  for (let i = 0; i < cypherMsg.length; i += 2) {
+    let pair = cypherMsg.slice(i, i + 2);
+
+    const pos1 = findLetterPosition(matrix, pair[0]);
+    const pos2 = findLetterPosition(matrix, pair[1]);
+
+    let decryptedPair = "";
+
+    if (pos1.row === pos2.row) {
+      decryptedPair += matrix[pos1.row][(pos1.col + 4) % 5];
+      decryptedPair += matrix[pos2.row][(pos2.col + 4) % 5];
+    } else if (pos1.col === pos2.col) {
+      decryptedPair += matrix[(pos1.row + 4) % 5][pos1.col];
+      decryptedPair += matrix[(pos2.row + 4) % 5][pos2.col];
+    } else {
+      decryptedPair += matrix[pos1.row][pos2.col];
+      decryptedPair += matrix[pos2.row][pos1.col];
+    }
+
+    plaintext += decryptedPair;
+  }
+
+  return plaintext.toLowerCase();
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/////// CIFRA DE HILL
+
+// Função para calcular o determinante de uma matriz
+function determinant(matrix) {
+  if (matrix.length === 2) {
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+  } else {
+    let det = 0;
+    for (let i = 0; i < matrix.length; i++) {
+      const minor = [];
+      for (let j = 1; j < matrix.length; j++) {
+        minor.push(matrix[j].slice(0, i).concat(matrix[j].slice(i + 1)));
+      }
+      det += matrix[0][i] * determinant(minor) * (i % 2 === 0 ? 1 : -1);
+    }
+    return det;
+  }
+}
+
+// Função para calcular o inverso modular de um número
+function modInverse(num, mod) {
+  for (let x = 1; x < mod; x++) {
+    if ((num * x) % mod === 1) {
+      return x;
+    }
+  }
+  return null; // Inverso modular não existe
+}
+
+// Função para encontrar a matriz inversa de uma matriz n x n
+function findMatrixInverse(matrix, mod) {
+  const det = determinant(matrix);
+  const modInv = modInverse(det, mod);
+
+  if (modInv === null) {
+    throw new Error('A matriz chave não é inversível.');
+  }
+
+  const n = matrix.length;
+  const adjugate = Array.from({ length: n }, () => Array(n));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const minor = [];
+      for (let row = 0; row < n; row++) {
+        if (row !== i) {
+          minor.push(matrix[row].filter((_, col) => col !== j));
+        }
+      }
+      adjugate[i][j] = determinant(minor) * ((i + j) % 2 === 0 ? 1 : -1);
+    }
+  }
+
+  // Calcule o inverso modular para cada elemento da matriz adjugada
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      adjugate[i][j] = (adjugate[i][j] * modInv) % mod;
+    }
+  }
+
+  return adjugate;
+}
+
+// Função para cifrar uma mensagem usando a Cifra de Hill
+export const encodeHill = (message, keyMatrix, mod) => {
+  if (message.length % keyMatrix.length !== 0) {
+    return 'ERRO: O tamanho da mensagem deve ser um múltiplo do tamanho da matriz chave.'
+  }
+
+  message = message.toUpperCase().replace(/[^A-Z]/g, ''); // Converter para maiúsculas e remover caracteres não alfabéticos
+  const n = keyMatrix.length;
+  let ciphertext = '';
+
+  for (let i = 0; i < message.length; i += n) {
+    const block = message.slice(i, i + n);
+    const blockVector = block.split('').map(char => char.charCodeAt(0) - 65); // Converter letras para números (A=0, B=1, ..., Z=25)
+    const resultVector = [];
+
+    for (let row = 0; row < n; row++) {
+      let sum = 0;
+      for (let col = 0; col < n; col++) {
+        sum += keyMatrix[row][col] * blockVector[col];
+      }
+      resultVector.push(sum % mod);
+    }
+
+    ciphertext += resultVector.map(num => String.fromCharCode(num + 65)).join(''); // Converter números de volta para letras
+  }
+
+  return ciphertext;
+}
+
+// Função para decifrar uma mensagem usando a Cifra de Hill
+export const decodeHill = (ciphertext, keyMatrix, mod) => {
+  if (ciphertext.length % keyMatrix.length !== 0) {
+    throw new Error('O tamanho da mensagem criptografada deve ser um múltiplo do tamanho da matriz chave.');
+  }
+
+  const inverseMatrix = findMatrixInverse(keyMatrix, mod);
+  const n = keyMatrix.length;
+  let plaintext = '';
+
+  for (let i = 0; i < ciphertext.length; i += n) {
+    const block = ciphertext.slice(i, i + n);
+    const blockVector = block.split('').map(char => char.charCodeAt(0) - 65); // Converter letras para números (A=0, B=1, ..., Z=25)
+    const resultVector = [];
+
+    for (let row = 0; row < n; row++) {
+      let sum = 0;
+      for (let col = 0; col < n; col++) {
+        sum += inverseMatrix[row][col] * blockVector[col];
+      }
+      resultVector.push((sum % mod + mod) % mod); // Lidar com números negativos
+    }
+
+    plaintext += resultVector.map(num => String.fromCharCode(num + 65)).join(''); // Converter números de volta para letras
+  }
+
+  return plaintext;
+}
